5e339920d8f1f9b905ff63f9b486b89defb7461b
thaissmartleiloes-prog <thais.smartleiloes@gmail.com>
2026-03-28 16:40:25 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        net: false,
+        fs: false,
+        child_process: false,
+        tls: false,
+      }
+    }
+    return config
+  },
 }
 module.exports = nextConfig
